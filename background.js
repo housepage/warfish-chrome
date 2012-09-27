2f85dfe50fdbe99728ef887ce305baa7038c90bc
Andrew Edward Gall <housepage@gmail.com>
2012-09-27 16:04:28 -0700

    Working prototype with minimal chrome code and using setInterval

--- background.js
+++ background.js
@@ -1,16 +1,5 @@
-var warfish = jQuery.jWarfish({});
-var polling_interval = 1000*5;
-
-function updateCount() {
-  count = count + 1;
-}
-
-
-function update() {
-  var count = warfish.getTurnGamesCount();
+function update_turn_count(count) {
   chrome.browserAction.setBadgeText({text:String(count)});
-
-  window.setTimeout(update,polling_interval);
 }
 
-window.onload = update;
+var warfish = jQuery.jWarfish({on_turn_update: update_turn_count});
